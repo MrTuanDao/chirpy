e6681d763bdf8605c3e798da801f648391cdacb1
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2025-08-14 13:27:51 +0000

    add index.html and logo

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Addr: ":8080",
-		Handler: serveMux,
+		Handler: mux,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
