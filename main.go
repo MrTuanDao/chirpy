9f4f7108e12bef7dbfe0f6feaac368748b6fb553
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2025-08-15 09:02:32 +0000

    some

--- main.go
+++ main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return next
+}
+
+func (cfg *apiConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	cfg := &apiConfig{}
+	fmt.Println(cfg.fileserverHits.Load())
+	mux.Handle("/app", http.StripPrefix("/app", cfg.middlewareMetricInc(http.FileServer(http.Dir(".")))))
+	mux.Handle("/metrics", cfg)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
